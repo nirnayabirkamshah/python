b54a6e640004bfbc5149906f765012d6348b28bb
nirnaya <nirnayabikramshah1@gmail.com>
2026-02-19 21:13:11 +0545

    Add files via upload

---
+++ for rectangle.py
@@ -0,0 +1,6 @@
+Length = (int(input("Enter the 	Length : ")))
+Width  =(int(input("Enter the 	Width : ")))
+area = 	Length * Width
+perimeter = 2*(Length + Width)
+print(f"{area}cm  is the area of the  rectangle ")
+print(f"{perimeter} cm is the perimeter of the  rectangle ")
