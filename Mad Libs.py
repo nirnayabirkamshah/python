77b50a42d24f2f0e248d4c1692e8694b9a13c655
nirnaya <nirnayabikramshah1@gmail.com>
2026-02-21 16:57:58 +0545

    Add files via upload

---
+++ Mad Libs.py
@@ -0,0 +1,19 @@
+race = str(input(" Enter your race : ")).lower().strip()
+age = int(input('  Enter your age  : '))
+
+if race == "african" :
+    print( " If you are african then , Nigg@R Where is your master!  ")
+elif race == "us":
+    print(" If you are form us then , I'm suing you for emotional distress and I'd like a side of ranch with that lawsuit. ")
+elif race == "eu":
+    print(" If you are from eu then , Your code is aesthetically pleasing, but it takes a 6-week vacation every August. ")
+elif race == "asian" :
+    print(" If you are asian then , Don't talk to me. Talk to the calculator I'm currently outperforming. ")
+if 13 <=  age <= 29 :
+    print("You're a  Teenager 🌱 ")
+elif 30 <= age <= 50 :
+    print("Unc 👴")
+elif  1 <= age <= 12:
+    print("gen Alpha twin 🥀")
+
+
