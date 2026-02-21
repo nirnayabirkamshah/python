d5b8910bbc010d6f7f5ee552a51d3571ac9fb6b1
nirnaya <nirnayabikramshah1@gmail.com>
2026-02-21 16:51:12 +0545

    Add files via upload

---
+++ account.py
@@ -0,0 +1,10 @@
+# --- Making a admin account ---
+username = input("Enter the username :")
+password = input("Enter the password :")
+if password == 'admin123' :
+    print("Access granted , Welcome  Back " , username )
+else:
+     print("Access invalid ")
+
+
+
